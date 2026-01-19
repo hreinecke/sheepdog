b3c7047931c75ee44edb3d611ef95a7efabd9a4e
Hannes Reinecke <hare@suse.de>
2026-01-19 16:28:48 +0100

    etcd: use joining node name in accept()

--- sheep/cluster/etcd.c
+++ sheep/cluster/etcd.c
@@ -1696,7 +1696,7 @@ static void etcd_handle_join(struct etcd_ctx *ctx,
 		sd_debug("node '%s' is master now, %d nodes, status %d",
 			 this_node.node_id, nr_nodes, cinfo.status);
 		cinfo_obj = json_object_new_object();
-		etcd_cinfo_to_json(&cinfo, cinfo_obj, &this_node);
+		etcd_cinfo_to_json(&cinfo, cinfo_obj, &joining);
 		etcd_update_event(ctx, EVENT_ACCEPT, obj);
 		json_object_put(cinfo_obj);
 	}
