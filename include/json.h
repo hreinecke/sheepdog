4ecb03942b5653a051a73b307cebab2e121a38b4
Hannes Reinecke <hare@suse.de>
2026-02-20 10:15:14 +0100

    sheep: convert epoch log to json format
    Convert the epoch log into json for better debugging.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

---
+++ include/json.h
@@ -0,0 +1,13 @@
+#ifndef __JSON_H__
+#define __JSON_H__
+
+#include <json-c/json.h>
+
+void node_to_json(struct sd_node *node, struct json_object *obj);
+void nodes_to_json(struct sd_node *nodes, int nr_nodes, json_object *obj);
+
+void json_to_node(struct json_object *obj, struct sd_node *node);
+void json_to_nodes(struct json_object *obj, struct sd_node *nodes,
+		   int *nr_nodes);
+
+#endif
