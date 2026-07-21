c11ee4791b28bd454b77b9230d2f20a02341da7f
Hannes Reinecke <hare@suse.de>
2026-07-21 09:58:28 +0200

    docker/start_cluster.sh: update to docker compose
    Update the start script to call 'docker compose' instead of
    hand-crafting docker commands.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- docker/start_cluster.sh
+++ docker/start_cluster.sh
@@ -1,18 +1,8 @@
 #!/bin/bash -x
 
-image=${IMAGE:-sheepdog:latest}
-nodes=${NODES:-"n1 n2 n3"}
-docker=${DOCKER:-docker}
+ENV="etcd-cluster.env"
+ETCD="etcd-cluster.yaml"
+SHEEP="sheep-cluster.yaml"
 
-echo "Killing old cluster..."
-for node in $nodes; do
-    $docker kill $node
-    $docker rm $node
-done
-
-sleep 1
-
-echo "Starting new cluster..."
-for node in $nodes; do
-    $docker run -h $node -d --name=$node -t $image
-done
+docker compose --env-file ${ENV} -f ${ETCD} up -d
+docker compose --env-file ${ENV} -f ${ETCD} up -d
