b7d2ca5d53d5a4f9b479aeaa11135872e7c3816d
Hannes Reinecke <hare@suse.de>
2026-07-23 11:08:45 +0200

    docker/start_cluster.sh: fixup script
    Fixup script to start sheep cluster correctly.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- docker/start_cluster.sh
+++ docker/start_cluster.sh
@@ -5,4 +5,4 @@ ETCD="etcd-cluster.yaml"
 SHEEP="sheep-cluster.yaml"
 
 docker compose --env-file ${ENV} -f ${ETCD} up -d
-docker compose --env-file ${ENV} -f ${ETCD} up -d
+docker compose --env-file ${ENV} -f ${SHEEP} up -d
