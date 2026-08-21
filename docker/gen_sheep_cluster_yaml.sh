ff27e0978dd25ccfb0a13dd346d7e9c23ed4e6e3
Hannes Reinecke <hare@suse.de>
2026-08-21 12:17:40 +0200

    docker: define iou-tgt containers
    Update the docker compose files to build and run ioutgt containers
    talking the the sheepdog cluster.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- docker/gen_sheep_cluster_yaml.sh
+++ docker/gen_sheep_cluster_yaml.sh
@@ -25,6 +25,7 @@ EOF
 for i in $(seq ${NUM_NODES}); do
     node=$(( $i - 1 ))
     port=$(( $node + 7000 ))
+    disc_port=$(( $node + 8000 ))
     echo "  sheep${node}:"
     echo "    image: sheepdog"
     echo "    hostname: \${SHEEP${node}_NAME}"
@@ -38,16 +39,23 @@ for i in $(seq ${NUM_NODES}); do
     echo "      - sheep${node}-data:\${SHEEP_STORE}"
     echo "    ports:"
     echo "      - \"127.0.0.1:${port}:7000\""
+    echo "      - \"127.0.0.1:${disc_port}:${disc_port}\""
     echo "    environment:"
     echo "      - SHEEP_CLUSTER_DRIVER=etcd:\${NODE${node}_IP}"
     echo "      - SHEEP_LOGGING=level=debug,dst=stdout"
     echo "      - SHEEP_BASE_DIR=\${SHEEP_BASE_DIR}"
     echo "      - SHEEP_ADDR=\${SHEEP${node}_IP}"
     echo "      - SHEEP_VNODES=\${SHEEP_VNODES}"
-    echo "      - SHEEP_ZONE=\${SHEEP_ZONE}"
+    echo "      - SHEEP_ZONE=\${SHEEP_ZONE:-${node}}"
     echo "      - SHEEP_JOURNAL=\${SHEEP_JOURNAL}"
     echo "      - VALGRIND=\${VALGRIND}"
     echo "    command: \${VALGRIND} /usr/sbin/sheep -f"
+    echo "  target${node}:"
+    echo "    image: ioutgt"
+    echo "    network_mode: service:sheep${node}"
+    echo "    security_opt:"
+    echo "      - seccomp=unconfined"
+    echo "    command: /usr/sbin/ioutgt-nvme-tcp --io-threads 4 --listen 0.0.0.0:${disc_port} --backend sheepdog:\${SHEEP${node}_IP}:7000"
 done
 
 echo
--- docker/start_cluster.sh
+++ docker/start_cluster.sh
@@ -5,7 +5,7 @@ SHEEP="sheep-cluster.yaml"
 DOG=../dog/dog
 
 for node in $(seq 0 4); do
-    SHEEP_ZONE=${node} docker compose --env-file ${ENV} -f ${SHEEP} up -d sheep${node}
+    docker compose --env-file ${ENV} -f ${SHEEP} up -d sheep${node}
 done
 
 $DOG cluster format -l
@@ -14,4 +14,10 @@ $DOG vdi create nqn.ns-1 64M
 $DOG vdi create nqn.ns-2 64M
 $DOG acl add vdi nqn.subsys-1 nqn.ns-1
 $DOG acl add vdi nqn.subsys-1 nqn.ns-2
+$DOG acl add member nqn.subsys-1 $(cat /etc/nvme/hostnqn)
 $DOG acl list -j | jq
+
+for node in $(seq 0 4); do
+    docker compose --env-file ${ENV} -f ${SHEEP} up -d target${node}
+done
+
