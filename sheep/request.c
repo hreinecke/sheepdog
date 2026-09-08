fea678be90a9eb18a5681dca48d0cf61f0934d44
Hannes Reinecke <hare@suse.de>
2026-09-08 14:20:16 +0200

    sheep/request: do not print 'unhandled error' for gateway_op_done()
    The gateway might not have the object, but that is nothing to worry
    about.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- sheep/request.c
+++ sheep/request.c
@@ -139,6 +139,7 @@ static void gateway_op_done(struct work *work)
 		}
 		break;
 	case SD_RES_SUCCESS:
+	case SD_RES_NO_OBJ:
 		break;
 	default:
 		sd_debug("unhandled error %s", sd_strerror(req->rp.result));
